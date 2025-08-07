fa6b487c1e8f49174bc3a4c812b7394cb5acbc25
Ankit Gope <ankitgopetatan@gmail.com>
2025-08-08 00:19:29 +0530

    Feat changes made backend - added models and routes as well

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,24 @@
 const express = require('express')
 // const bodyParser = require('body-parser')
-// const cors = require('cors')
+const cors = require('cors')
+const env = require('dotenv')
+const connectDB = require('./config/db')
+require("dotenv").config();
+
+const app = express()
+connectDB()
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// basic route
+app.get('/',(req,res)=>{
+    res.send('Welcome to the backend server!')
+})
+
+// start server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
